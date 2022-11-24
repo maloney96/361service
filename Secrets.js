795859ffa3d8173cc7150a054133e41b7da42c01
Liam Maloney <malonlia@oregonstate.edu>
2022-11-23 20:35:04 -0500

    Updated with my secrets

--- Secrets.js
+++ Secrets.js
@@ -1,4 +1,4 @@
-var positionStackAccessKey = "";
-var weatherbitAccessKey = "";
-var PORT = 3000;
+var positionStackAccessKey = "451a3cb482248f8771b098e65dff3bbd";
+var weatherbitAccessKey = "07b69b57e0649225bafb2b2d20ca6b4c";
+var PORT = 3001;
 module.exports = { PORT, positionStackAccessKey, weatherbitAccessKey };
